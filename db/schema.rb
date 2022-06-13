1e5e8cc7f973a3ea609273010bfd620e79167d88
finn stuerenburg <finnstueren@gmail.com>
2022-06-13 19:18:18 +0200

    removed user model, user to be replaced by devise user

--- db/schema.rb
+++ db/schema.rb
@@ -27,20 +27,5 @@ ActiveRecord::Schema.define(version: 2022_06_13_170849) do
     t.index ["user_id"], name: "index_patientenverfuegungs_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "birthdate"
-    t.string "email"
-    t.string "city"
-    t.integer "plz"
-    t.string "password"
-    t.string "street"
-    t.string "country"
-    t.date "date_of_birth"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   add_foreign_key "patientenverfuegungs", "users"
 end
